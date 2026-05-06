31a50cf69ee84a147388852745bb167e7f7ebd1d
NhatNguyen <nhatnguyen27042005@gmail.com>
2026-05-06 14:36:43 +0700

    chore: initial push workspace

---
+++ src/main/java/com/omniflow/backend/OmniflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.omniflow.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniflowBackendApplication.class, args);
+	}
+
+}
